04a6c6e0ad4df8c4504cddbbb5747cd00cd05c2f
Peter.Bucher <Peter.Bucher@localhost>
2009-09-26 22:16:42 +0000

    Fixed integration project.

---
+++ PeterBucher.AutoFunc.Integration.Web/IContainerAccessor.cs
@@ -0,0 +1,13 @@
+﻿namespace PeterBucher.AutoFunc.Integrations.Web
+{
+    /// <summary>
+    /// Represents an accessor for <see cref="IContainer" />.
+    /// </summary>
+    public interface IContainerAccessor
+    {
+        /// <summary>
+        /// Gets the container.
+        /// </summary>
+        IContainer Container { get; }
+    }
+}
