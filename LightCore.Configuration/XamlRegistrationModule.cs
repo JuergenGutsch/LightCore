02d5eea7354bac52227632fcf88ac21a16a9c6fb
peterbucher <peterbucher@localhost>
2009-10-27 13:41:51 +0000

    Experimental support for other config sources (filepath or a stream), not tested yet.

--- LightCore.Configuration/XamlRegistrationModule.cs
+++ LightCore.Configuration/XamlRegistrationModule.cs
@@ -1,17 +1,53 @@
-﻿namespace LightCore.Configuration
+﻿using System.IO;
+using System.Windows.Markup;
+
+namespace LightCore.Configuration
 {
     /// <summary>
     /// Represents a xaml registration module for LightCore.
     /// </summary>
     public class XamlRegistrationModule : RegistrationModule
     {
+        private LightCoreConfiguration _configuration;
+
+        ///<summary>
+        /// Initializes a new instance of <see cref="XamlRegistrationModule" />.
+        ///</summary>
+        public XamlRegistrationModule()
+        {
+            _configuration = LightCoreConfiguration.Instance;
+        }
+
+        ///<summary>
+        /// Initializes a new instance of <see cref="XamlRegistrationModule" />.
+        ///</summary>
+        ///<param name="configPath">The path to a xaml config file.</param>
+        public XamlRegistrationModule(string configPath)
+        {
+            using (var file = File.Open(configPath, FileMode.Open))
+            {
+                _configuration = (LightCoreConfiguration)XamlReader.Load(file);
+            }
+        }
+
+        ///<summary>
+        /// Initializes a new instance of <see cref="XamlRegistrationModule" />.
+        ///</summary>
+        ///<param name="configurationStream">The stream containing the configuration file content.</param>
+        public XamlRegistrationModule(Stream configurationStream)
+        {
+            _configuration = (LightCoreConfiguration)XamlReader.Load(configurationStream);
+        }
+
         /// <summary>
         /// Registers all candidates.
         /// </summary>
         /// <param name="containerBuilder">The controllerbuilder.</param>
         public override void Register(IContainerBuilder containerBuilder)
         {
-            RegistrationLoader.Instance.Register(containerBuilder, LightCoreConfiguration.Instance);
+            RegistrationLoader
+                .Instance
+                .Register(containerBuilder, LightCoreConfiguration.Instance);
         }
     }
 }
