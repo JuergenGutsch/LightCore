8bfa7f044e2ccb1965baee3d3a9b644836629f66
peterbucher <peterbucher@localhost>
2010-01-02 04:23:04 +0000

    Fixed SRP issue with extensionmethods.

---
+++ LightCore/ExtensionMethods/System/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LightCore.ExtensionMethods.System
+{
+    /// <summary>
+    /// Represents extensionmethods for <see cref="String" /> type.
+    /// </summary>
+    internal static class StringExtensions
+    {
+        /// <summary>
+        /// Formats a string with given arguments.
+        /// </summary>
+        /// <param name="source">The format string.</param>
+        /// <param name="values">The values.</param>
+        /// <returns>The formatted string.</returns>
+        internal static string FormatWith(this string source, params object[] values)
+        {
+            return string.Format(source, values);
+        }
+    }
+}
--- LightCore/ExtensionMethods/System/TypeExtensions.cs
+++ LightCore/ExtensionMethods/System/TypeExtensions.cs
@@ -3,21 +3,10 @@
 namespace LightCore.ExtensionMethods.System
 {
     /// <summary>
-    /// Represents extensionmethods for System namespace.
+    /// Represents extensionmethods for <see cref="Type" /> type.
     /// </summary>
     internal static class SystemExtensions
     {
-        /// <summary>
-        /// Formats a string with given arguments.
-        /// </summary>
-        /// <param name="source">The format string.</param>
-        /// <param name="values">The values.</param>
-        /// <returns>The formatted string.</returns>
-        internal static string FormatWith(this string source, params object[] values)
-        {
-            return string.Format(source, values);
-        }
-
         /// <summary>
         /// Checks whether the type is concrete or not.
         /// </summary>
