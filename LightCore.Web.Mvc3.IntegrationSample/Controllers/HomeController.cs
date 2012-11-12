c35d752c79727d5d36aecb80a572940d68f898d3
Peter Bucher <peter.j.bucher@gmail.com>
2012-11-13 00:40:31 +0100

    MVC Sample Fix.

---
+++ LightCore.Web.Mvc3.IntegrationSample/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using System.Web.Mvc;
+using LightCore.TestTypes;
+
+namespace LightCore.Web.Mvc3.IntegrationSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IFoo _foo;
+
+        public HomeController(IFoo foo)
+        {
+            this._foo = foo;
+        }
+
+        public ActionResult Index()
+        {
+            if(this._foo != null)
+            {
+                ViewBag.Message = "ASP.NET MVC 3.0 - funktioniert. (In ASP.NET MVC 4.0 wird LightCore gleich angewandt wie in diesem Beispiel.";
+            }
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
