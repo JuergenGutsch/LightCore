2249448636ebc7196afdba44c99345f3d93179af
juergengutsch <juergen@gutsch-online.de>
2016-01-05 18:12:02 +0100

    start implementing console.core as an example application

--- LightCore.ConsoleClient.Core/Program.cs
+++ LightCore.ConsoleClient.Core/Program.cs
@@ -1,9 +1,31 @@
-﻿namespace LightCore.ConsoleClient.Core
+﻿using System;
+using LightCore.Configuration;
+using LightCore.TestTypes;
+
+namespace LightCore.ConsoleClient.Core
 {
     public class Program
     {
         public void Main(string[] args)
         {
+            var builder = new ContainerBuilder();
+
+            var module = new JsonRegistrationModule();
+
+            builder.Register("Test");
+
+            builder.RegisterModule(module);
+
+            builder.Register<IBar, Bar>();
+
+            var container = builder.Build();
+
+            var func = container.Resolve<Func<string>>();
+            var test = func();
+
+            var namedScreen = container.Resolve<WelcomeScreen>();
+            namedScreen.WriteText();
+
         }
     }
 }
