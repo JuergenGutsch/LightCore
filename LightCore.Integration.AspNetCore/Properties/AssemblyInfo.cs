a998b7a25d7edde8efaa4dd6b65dc9c84266e29a
juergengutsch <juergen@gutsch-online.de>
2017-06-27 18:26:54 +0200

    Add CommonServiceLocator implementation

--- LightCore.Integration.AspNetCore/Properties/AssemblyInfo.cs
+++ LightCore.Integration.AspNetCore/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
-﻿using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("fb5bc883-c28b-43c7-ae22-8283904f8345")]
