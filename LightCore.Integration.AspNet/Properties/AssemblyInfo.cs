1e6d9ba8b38525e84ba91104dafb41df221efbfb
juergengutsch <juergen@gutsch-online.de>
2017-07-10 17:45:33 +0200

    add web integration projects

---
+++ LightCore.Integration.AspNet/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LightCore.Integration.Web")]
+[assembly: AssemblyDescription("ASP.NET (Web) integration for LightCore.")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("939bd961-c596-4e27-bf39-d2655a8857ff")]
+
+[assembly: InternalsVisibleTo("LightCore.Integration.Web.Tests")]
