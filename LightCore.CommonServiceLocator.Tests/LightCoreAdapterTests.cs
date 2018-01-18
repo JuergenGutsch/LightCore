0cb7a2e8bbbb3d7b34232b36f9ea8851084247e0
juergengutsch <juergen@gutsch-online.de>
2018-01-18 20:23:58 +0100

    update dependencies

--- LightCore.CommonServiceLocator.Tests/LightCoreAdapterTests.cs
+++ LightCore.CommonServiceLocator.Tests/LightCoreAdapterTests.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using CommonServiceLocator;
 using FluentAssertions;
-using Microsoft.Practices.ServiceLocation;
 
 using Xunit;
 
--- LightCore.CommonServiceLocator/LightCoreAdapter.cs
+++ LightCore.CommonServiceLocator/LightCoreAdapter.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.Practices.ServiceLocation;
+using CommonServiceLocator;
 
 namespace LightCore.CommonServiceLocator
 {
